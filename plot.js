a049613ee55ce07f5109116968a8c7714e1b8e29
Yeryomin1 <48363433+Yeryomin1@users.noreply.github.com>
2020-08-24 22:05:11 +0300

    global constants removed

--- plot.js
+++ plot.js
@@ -1,9 +1,11 @@
 
-const ORIGIN_Y = 20;
-const ORIGIN_X = 40;
+//const ORIGIN_Y = 20;
+//const ORIGIN_X = 40;
 
 let plot = {};
 plot.maxVal = 20;
+plot.originX = 40;
+plot.originY = 20;
 
 
 plot.draw = function (data, steps, context) {
@@ -40,15 +42,15 @@ plot.drawAxisX = function (stepsNumber) {
     plot.ctx.strokeStyle = "black";
     plot.ctx.lineCap = "butt";
     //axis line:
-    plot.ctx.moveTo(0, CANVAS_HEIGHT - ORIGIN_Y);
-    plot.ctx.lineTo(CANVAS_WIDTH, CANVAS_HEIGHT - ORIGIN_Y);
+    plot.ctx.moveTo(0, CANVAS_HEIGHT - plot.originY);
+    plot.ctx.lineTo(CANVAS_WIDTH, CANVAS_HEIGHT - plot.originY);
     plot.ctx.stroke();
     //score:
-    let scoreStep = (CANVAS_WIDTH - ORIGIN_X) / 6;
+    let scoreStep = (CANVAS_WIDTH - plot.originX) / 6;
     let labelLength = 8;
     for (let i = 1; i < 6; i++) {
-        plot.ctx.moveTo(ORIGIN_X + i * scoreStep, CANVAS_HEIGHT - ORIGIN_Y - labelLength / 2);
-        plot.ctx.lineTo(ORIGIN_X + i * scoreStep, CANVAS_HEIGHT - ORIGIN_Y + labelLength / 2);
+        plot.ctx.moveTo(plot.originX + i * scoreStep, CANVAS_HEIGHT - plot.originY - labelLength / 2);
+        plot.ctx.lineTo(plot.originX + i * scoreStep, CANVAS_HEIGHT - plot.originY + labelLength / 2);
         plot.ctx.stroke();
     }
     //text labels:
@@ -57,7 +59,7 @@ plot.drawAxisX = function (stepsNumber) {
     plot.ctx.fillStyle = "black";
     plot.ctx.font = "10pt Arial";
     for (let i = 1; i < 6; i++) {
-        plot.ctx.fillText(displacement + i * plot.xRange / 5, ORIGIN_X + i * scoreStep - 5, CANVAS_HEIGHT - ORIGIN_Y + labelLength / 2 + 12);
+        plot.ctx.fillText(displacement + i * plot.xRange / 5, plot.originX + i * scoreStep - 5, CANVAS_HEIGHT - plot.originY + labelLength / 2 + 12);
     }
 
 
@@ -69,40 +71,40 @@ plot.drawAxisY = function () {
     plot.ctx.strokeStyle = "black";
     plot.ctx.lineCap = "butt";
     //axis line:
-    plot.ctx.moveTo(ORIGIN_X, CANVAS_HEIGHT);
-    plot.ctx.lineTo(ORIGIN_X, CANVAS_HEIGHT - PLOT_HEIGHT);
+    plot.ctx.moveTo(plot.originX, CANVAS_HEIGHT);
+    plot.ctx.lineTo(plot.originX, CANVAS_HEIGHT - PLOT_HEIGHT);
     plot.ctx.stroke();
     //score:
     let labels = 5;
-    let scoreStep = (PLOT_HEIGHT - ORIGIN_Y) / labels;
+    let scoreStep = (PLOT_HEIGHT - plot.originY) / labels;
     let labelLength = 8;
     for (let i = 1; i < labels; i++) {
-        plot.ctx.moveTo(ORIGIN_X - labelLength / 2, CANVAS_HEIGHT - ORIGIN_Y - i * scoreStep);
-        plot.ctx.lineTo(ORIGIN_X + labelLength / 2, CANVAS_HEIGHT - ORIGIN_Y - i * scoreStep);
+        plot.ctx.moveTo(plot.originX - labelLength / 2, CANVAS_HEIGHT - plot.originY - i * scoreStep);
+        plot.ctx.lineTo(plot.originX + labelLength / 2, CANVAS_HEIGHT - plot.originY - i * scoreStep);
         plot.ctx.stroke();
     }
     //text labels:
     plot.ctx.fillStyle = "black";
     plot.ctx.font = "10pt Arial";
     for (let i = 1; i < labels; i++) {
-        plot.ctx.fillText(i * plot.yRange / labels, 10, CANVAS_HEIGHT - ORIGIN_Y - i * scoreStep + 5);
+        plot.ctx.fillText(i * plot.yRange / labels, 10, CANVAS_HEIGHT - plot.originY - i * scoreStep + 5);
     }
 
 }
 
 plot.drawData = function (dataArr, stepsNum) {
-    let columnWidth = (CANVAS_WIDTH - ORIGIN_X) / 120;
+    let columnWidth = (CANVAS_WIDTH - plot.originX) / 120;
     plot.ctx.beginPath();
     plot.ctx.lineWidth = 5;
     plot.ctx.strokeStyle = "red";
     plot.ctx.lineCap = "butt";
-    let zoom = (PLOT_HEIGHT - ORIGIN_Y) / plot.yRange;
+    let zoom = (PLOT_HEIGHT - plot.originY) / plot.yRange;
 
     for (let i = 1; i <= stepsNum; i++) {
         //data point column line:
 
-        plot.ctx.moveTo(ORIGIN_X + i * columnWidth, CANVAS_HEIGHT - ORIGIN_Y);
-        plot.ctx.lineTo(ORIGIN_X + i * columnWidth, CANVAS_HEIGHT - ORIGIN_Y - zoom * dataArr[i - 1]);
+        plot.ctx.moveTo(plot.originX + i * columnWidth, CANVAS_HEIGHT - plot.originY);
+        plot.ctx.lineTo(plot.originX + i * columnWidth, CANVAS_HEIGHT - plot.originY - zoom * dataArr[i - 1]);
         plot.ctx.stroke();
     }
 
