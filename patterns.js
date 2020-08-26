191dd28ac496b7a12b62f93efe2e5713f9936b08
Yeryomin1 <48363433+Yeryomin1@users.noreply.github.com>
2020-08-26 20:09:47 +0300

    Grosper glider gun pattern added

--- patterns.js
+++ patterns.js
@@ -23,6 +23,65 @@ patterns.smallExploder = function (array, xPosition, yPosition) {
     array[x][y - 1] = 1;
 }
 
+patterns.gliderGun = function (array, xPosition, yPosition) {
+    x = xPosition || (WORLD_WIDTH * 0.5 - 2);
+    y = yPosition || (WORLD_HEIGHT * 0.5 - 2);
+
+    //square 1:
+    array[x - 18][y + 6] = 1;
+    array[x - 17][y + 6] = 1;
+    array[x - 18][y + 5] = 1;
+    array[x - 17][y + 5] = 1;
+
+    //central:
+    array[x - 8][y + 6] = 1;
+    array[x - 8][y + 5] = 1;
+    array[x - 8][y + 4] = 1;
+
+    array[x - 7][y + 7] = 1;
+    array[x - 7][y + 3] = 1;
+
+    array[x - 6][y + 8] = 1;
+    array[x - 6][y + 2] = 1;
+
+    array[x - 5][y + 8] = 1;
+    array[x - 5][y + 2] = 1;
+
+    array[x - 4][y + 5] = 1;
+
+    array[x - 3][y + 7] = 1;
+    array[x - 3][y + 3] = 1;
+
+    array[x - 2][y + 6] = 1;
+    array[x - 2][y + 5] = 1;
+    array[x - 2][y + 4] = 1;
+
+    array[x - 1][y + 5] = 1;
+
+    //right:
+    array[x + 2][y + 8] = 1;
+    array[x + 2][y + 7] = 1;
+    array[x + 2][y + 6] = 1;
+
+    array[x + 3][y + 8] = 1;
+    array[x + 3][y + 7] = 1;
+    array[x + 3][y + 6] = 1;
+
+    array[x + 4][y + 5] = 1;
+    array[x + 4][y + 9] = 1;
+
+    array[x + 6][y + 4] = 1;
+    array[x + 6][y + 5] = 1;
+    array[x + 6][y + 9] = 1;
+    array[x + 6][y + 10] = 1;
+
+    //sruare 2:
+    array[x + 16][y + 7] = 1;
+    array[x + 16][y + 8] = 1;
+    array[x + 17][y + 7] = 1;
+    array[x + 17][y + 8] = 1;
+}
+
 
 patterns.clear = function (array) {
     for (i = 0; i < WORLD_WIDTH; i++)
